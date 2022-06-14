03f8812d8b808902dc95b9c29acf4ef4a40574e2
Livne Lang <livnelang@gmail.com>
2022-06-14 10:05:02 +0300

    af

--- src/services/API.ts
+++ src/services/API.ts
@@ -5,11 +5,11 @@ import { RecipeCreateParams } from "../components/UploadRecipe/UploadRecipe";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 class APIService {
   public async getItems(): Promise<Recipes> {
-    return axios.get(BASE_URL + "recipes");
+        return axios.get(BASE_URL + "recipes");
   }
 
   public async createRecipe(body: RecipeCreateParams): Promise<Recipe> {
-                return axios.post(BASE_URL + "recipes", body).then((res) => res.data);
+                    return axios.post(BASE_URL + "recipes", body).then((res) => res.data);
   }
 }
 
